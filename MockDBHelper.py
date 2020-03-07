0c7b83c6b7fbcc4d61c3a9faf8c2be4273ef949e
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-03-07 15:08:32 +0500

    input-injection

--- MockDBHelper.py
+++ MockDBHelper.py
@@ -2,14 +2,14 @@ class MockDBHelper:
     def connect(self, database='crimemap'):
         pass
 
-    def get_all_inputs(self):
-        return []
+    # def get_all_inputs(self):
+    #     return []
 
-    def add_input(self, data):
-        pass
-
-    def clear_all(self):
-        pass
+    # def add_input(self, data):
+    #     pass
+    #
+    # def clear_all(self):
+    #     pass
 
     def add_crime(self, category, date, description):
         pass
--- crimemap/crimemap.py
+++ crimemap/crimemap.py
@@ -2,53 +2,80 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import json
+import datetime, string
+import dateparser
 
 # релаьная БД
-# from dbhelper import DBHelper
+from dbhelper import DBHelper
 # пока что работаем с имитацией БД
 # макет бд
 import db_config
 
-if db_config.test:
-    from MockDBHelper import MockDBHelper as DBHelper
-else:
-    from dbhelper import DBHelper
+# if db_config.test:
+#     from MockDBHelper import MockDBHelper as DBHelper
+# else:
+#     from dbhelper import DBHelper
 # макет бд, end
 app = Flask(__name__)
 DB = DBHelper()
 
+categories = ['magging', 'break-in']
+
+
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, "%Y-%m-%d")
+    except TypeError:
+        return None
+
+
+def sanitize_string(userinput):
+    whitelist = string.ascii_letters + string.digits + " !?$.,;:-'()&"
+    return filter(lambda x: x in whitelist, userinput)
+
 
 @app.route("/")
-def home():
+def home(error_message=None):
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
-    return render_template("home.html", crimes=crimes)
+    return render_template("home.html", crimes=crimes, categories=categories, error_message=error_message)
 
 
-@app.route("/add", methods=["POST"])
-def add():
-    try:
-        data = request.form.get("userinput")
-        DB.add_input(data)
-    except Exception as e:
-        print(e)
-    return home()
+#
+# @app.route("/add", methods=["POST"])
+# def add():
+#     try:
+#         data = request.form.get("userinput")
+#         DB.add_input(data)
+#     except Exception as e:
+#         print(e)
+#     return home()
 
-
-@app.route("/clear")
-def clear():
-    try:
-        DB.clear_all()
-    except Exception as e:
-        print(e)
-    return home()
+#
+# @app.route("/clear")
+# def clear():
+#     try:
+#         DB.clear_all()
+#     except Exception as e:
+#         print(e)
+#     return home()
 
 
 @app.route("/submitcrime", methods=["POST"])
 def submitcrime():
+    # try:
+    #     latitude = float(request.form.get("latitude"))
+    #     longitude = float(request.form.get("longitude"))
+    # except ValueError:
+    #     return home()
     category = request.form.get("category")
-    date = request.form.get("date")
-    description = request.form.get("description")
+    if category not in categories:
+        return home()
+    date = format_date(request.form.get("date"))
+    if not date:
+        return home("Invalid date. Please use yyyy-mm--dd format")
+    description = sanitize_string(request.form.get("description"))
     DB.add_crime(category, date, description)
     return home()
 
